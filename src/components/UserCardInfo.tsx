e5881779fce8370a6088e8f61c29540837a1de40
HgaMen <hga.int2021@gmail.com>
2023-05-26 11:49:57 -0600

    finish pero no finish

--- src/components/UserCardInfo.tsx
+++ src/components/UserCardInfo.tsx
@@ -1,53 +1,108 @@
+// import Image from "next/image";
+import GithubIcon from "@/components/icons/GithubIcon";
+import LocationIcon from "@/components/icons/LocationIcon";
+import LinkIcon from "@/components/icons/LinkIcon";
+import TwitterIcon from "@/components/icons/TwitterIcon";
+import BuildingIcon from "@/components/icons/BuildingIcon";
+import { User } from "@/interfaces/user";
 import Image from "next/image";
-import GithubLogo from "@/components/icons/GithubIcon";
 
-const UserCardInfo = () => {
+interface Props {
+    user: User;
+}
+
+function valideURL(url: string) {
+    // Verificar se la URL comienza con "http://" o "https://"
+    if (!/^https?:\/\//i.test(url)) {
+        // Agregar "https://" al principio de la URL
+        url = "https://" + url;
+    }
+    return url;
+}
+
+const UserCardInfo = ({ user }: Props) => {
     return (
-        <article className="grid-areas rounded-xl bg-blue-900 p-2 text-white">
-            <div className="section-logo h-24 w-24 place-content-center rounded-full bg-gray-200 p-1 file:grid">
-                <GithubLogo className="relative top-2 h-full w-full" />
+        <article className="grid-areas rounded-xl bg-blue-900 p-4 text-white">
+            <div className="section-logo mr-3 grid h-24 w-24 place-content-center overflow-hidden rounded-full bg-gray-200 p-1  lg:mx-auto">
+                <Image
+                    src={user.avatar_url}
+                    width={96}
+                    height={96}
+                    alt={`profile img user ${user.name}`}
+                    className="rounded-full"
+                />
+                {/* <GithubIcon className="relative top-2 h-full w-full" /> */}
             </div>
             <div className="section-title">
-                <h2>The Octocat</h2>
-                <p>@octocat</p>
+                <h2 className="text-3xl font-bold">{user.name}</h2>
+                <p>@{user.login}</p>
             </div>
-            <p className="section-date">Joined 25 Jan 2011</p>
-            <p className="section-description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                totam, earum labore molestiae sequi eos sunt recusandae, illo
-                aperiam nihil, repudiandae perferendis ipsa veritatis velit
-                officiis enim obcaecati incidunt adipisci!
+            <p className="section-date lg:text-right">
+                {new Date(user.created_at || "").toLocaleDateString("es-ES", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                })}
             </p>
-            <div className="section-number flex justify-around bg-blue-950">
+            <p className="section-description mt-8 leading-loose">
+                {user.bio || "Sin user bio"}
+            </p>
+            <div className="section-number mt-4 flex justify-around rounded-xl bg-blue-950 p-8 text-center">
                 <article>
                     <p>Repos</p>
-                    <p>8</p>
+                    <p className="text-xl font-bold">{user.public_repos}</p>
                 </article>
                 <article>
                     <p>Followers</p>
-                    <p>3938</p>
+                    <p className="text-xl font-bold">{user.followers}</p>
                 </article>
                 <article>
                     <p>Following</p>
-                    <p>9</p>
+                    <p className="text-xl font-bold">{user.following}</p>
                 </article>
             </div>
-            <div className="section-social md:grid md:grid-cols-2">
-                <article>
-                    <i>Icono</i>
-                    <p>San Francisco</p>
+            <div className="section-social mt-4 space-y-3 md:grid md:grid-cols-2">
+                <article className="flex space-x-2">
+                    <i>
+                        <LocationIcon
+                            className="h-full w-full fill-white"
+                            width={"1rem"}
+                        />
+                    </i>
+                    <span>{user.location}</span>
                 </article>
-                <article>
-                    <i>Icono</i>
-                    <p>San Francisco</p>
+                <article className="flex space-x-2">
+                    <i>
+                        <LinkIcon
+                            className="h-full w-full fill-white"
+                            width={"1rem"}
+                        />
+                    </i>
+                    <a href={valideURL(user?.blog)} className="truncate">
+                        {user?.blog || "not information"}
+                    </a>
                 </article>
-                <article>
-                    <i>Icono</i>
-                    <p>San Francisco</p>
+                <article className="flex space-x-2">
+                    <i>
+                        <TwitterIcon
+                            className="h-full w-full fill-white"
+                            width={"1rem"}
+                        />
+                    </i>
+                    <a
+                        href={`https://www.twitter.com/${user?.twitter_username}`}
+                    >
+                        {user.twitter_username}
+                    </a>
                 </article>
-                <article>
-                    <i>Icono</i>
-                    <p>San Francisco</p>
+                <article className="flex space-x-2">
+                    <i>
+                        <BuildingIcon
+                            className="h-full w-full fill-white"
+                            width={"1rem"}
+                        />
+                    </i>
+                    <span>{user?.company || "not information"}</span>
                 </article>
             </div>
         </article>
