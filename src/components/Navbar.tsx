59f539da9626fdcb45e1d124207b0e6f4c1bc023
HgaMen <hga.int2021@gmail.com>
2023-05-27 13:36:57 -0600

    fixed typeof window

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,9 +8,12 @@ const initialThemeState = () => {
     if (localStorage.getItem("theme")) {
         return localStorage.getItem("theme") as "light" | "dark";
     }
-    return window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light";
+    if (typeof window !== "undefined") {
+        return window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light";
+    }
+    return "dark";
 };
 
 // const initialThemeState = localStorage.getItem("theme")
